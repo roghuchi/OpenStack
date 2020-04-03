4456c22ec638705b5f5608ed94fa4068e0b6ddf8
Roghuchi <38735518+roghuchi@users.noreply.github.com>
2020-04-04 00:21:59 +0430

    Create getbackups.py

---
+++ getbackups.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+import os
+import subprocess
+import datetime
+import time
+
+#List of Instances ID=====
+
+Command=" nova list --minimal | awk -F'|' '/\|/ && !/ID/{system(\"echo \"$2\"\")}'"
+ListOfInstances = subprocess.Popen([Command], stdout=subprocess.PIPE, shell=True)
+(output, err) = ListOfInstances.communicate()
+output=str(output)
+output=output.replace("b'","")
+output=output.replace("'"," ")
+output=output.replace(chr(92)+"n",' ')
+ListOfInstances=output.split()
+
+#Backup=====
+#BackupType can be "daily" or "weekly" too 
+BackupType="manual"
+#Int parameter representing how many backups to keep around
+Rotation="7"
+
+ListOfOutputs = []
+log = ""
+for ID in ListOfInstances:
+    #Name of the backup image
+    Command = "echo 'Start OpenStack snapshot creation for instance ID'"+ID
+    os.system(Command)
+    Now = datetime.datetime.now()
+    Now=str(Now)
+    Now = Now.replace(" ","-")
+    NameOfBackupImage=Now+ID
+    Command ="nova backup "+ ID +" "+NameOfBackupImage+" "+ BackupType +" "+Rotation
+    x = subprocess.Popen([Command], stdout=subprocess.PIPE, shell=True)
+    (output, err) = x.communicate()
+    output = str (output)
+    output=output.replace("b'","")
+    log = output + log + "\n"
+    time.sleep(3)
+
+
+#Report=====
+EmailFrom = "senderemail"
+EmailTo = "youremail"
+
+#Mail must be pre-installed
+Command = " mail -s \"Logs of backups instances\" -r \"" + EmailFrom + "\" \"" + EmailTo + "\""
+os.system(Command)
